5b1c9f862e2c4b1395a2d63359c28b30a3dc569a
Dmitry Trofimchuk <dimitrez@gmail.com>
2017-06-19 15:31:17 +0300

    deleted old sh

--- zabbix-30-ubuntu-14-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd /usr/src
-wget http://repo.zabbix.com/zabbix/3.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_3.0-1+trusty_all.deb
-dpkg -i zabbix-release_3.0-1+trusty_all.deb
-apt-get update
-apt-get install zabbix-agent
-
